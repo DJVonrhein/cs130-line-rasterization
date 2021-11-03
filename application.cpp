675cc85ba1dd5bfc3967b407edce7191da02cb39
Daniel Vonrhein <danjvon@gmail.com>
2021-11-03 02:10:09 -0700

    fixed divide by 0 issue and fixed slope calculation in if statement draw_line

--- application.cpp
+++ application.cpp
@@ -42,7 +42,10 @@ void set_pixel(int x, int y)
 void draw_line(int x0, int y0, int x1, int y1, float col[3])
 {
     // TODO: Implement the correct algorithm here.
-    if((y1 - y0)/(x1 - 0) <= 1 && (y1 - y0)/(x1 - 0) >= -1 ){   // |m| <= 1
+    if (x1 - x0 == 0)
+        return;
+
+    if((y1 - y0)/(x1 - x0) <= 1 && (y1 - y0)/(x1 - x0) >= -1 ){   // |m| <= 1
 
         if(x1 < x0){        //swap the points if necessary
             int temp = x0;
