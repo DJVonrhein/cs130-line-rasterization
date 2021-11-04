70757f8252d0fbb3af09392f9653650369a6093a
Daniel Vonrhein <danjvon@gmail.com>
2021-11-04 15:03:02 -0700

    testing somethign

--- application.cpp
+++ application.cpp
@@ -56,7 +56,7 @@ void draw_line(int x0, int y0, int x1, int y1, float col[3])
 
         // float g = 2 *( (y0 - y1)* (x0 + 1) + (x1 - x0) * (y0 + 0.5) + (x0 * y1 )- (x1 * y0)); // Ax + By + C
         
-        float g =  2* ( (y0 - y1)* (x0 + 1) + (x1 - x0) * (y0 + 0.5) + (x0 * y1 )- (x1 * y0)); // Ax + By + C
+        // float g =  2* ( (y0 - y1)* (x0 + 1) + (x1 - x0) * (y0 + 0.5) + (x0 * y1 )- (x1 * y0)); // Ax + By + C
         
         // float dx = x1 - x0;
         float dy = (y1 > y0) ? 1.0 : -1.0;
@@ -67,10 +67,10 @@ void draw_line(int x0, int y0, int x1, int y1, float col[3])
         for(int x = x0, y = y0; x <= x1; x++)
         {
             set_pixel(x, y, col);
+            int g = 2* ( (y0 - y1)* (x) + (x1 - x0) * (y ) + (x0 * y1 )- (x1 * y0));
             if ( g < 0){
                 y += dy;
             }
-            g = 2* ( (y0 - y1)* (x) + (x1 - x0) * (y ) + (x0 * y1 )- (x1 * y0));
             
         }
     }
@@ -83,7 +83,6 @@ void draw_line(int x0, int y0, int x1, int y1, float col[3])
 
 
         // float g = 2 *( (x0 - x1)* (y0 + 1) + (y1 - y0) * (x0 + 0.5) + (y0 * x1 )- (y1 * x0)); // Ax + By + C
-        float g = 2 * ( (y0 - y1)* (x0 + 1) + (x1 - x0) * (y0 + 0.5) + (x0 * y1 )- (x1 * y0)); // Ax + By + C
         // float dy = y1 - y0;
         float dx = (x1 > x0) ? 1.0 : -1.0;
         // float m = dy/dx;
@@ -93,10 +92,10 @@ void draw_line(int x0, int y0, int x1, int y1, float col[3])
         for(int x = x0, y = y0; y <= y1; y++)
         {
             set_pixel(x, y, col);
+            int g = 2* ( (y0 - y1)* (x) + (x1 - x0) * (y) + (x0 * y1 )- (x1 * y0));
             if ( g < 0){
                 x += dx;
             }
-            g = 2* ( (y0 - y1)* (x) + (x1 - x0) * (y) + (x0 * y1 )- (x1 * y0));
         }
     }
 }
